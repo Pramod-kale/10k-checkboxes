6af13006ba57347cbba1ffc0ebdea0ba836995ee
pramod kale <pramodkale.sit@gmail.com>
2024-10-03 20:02:57 +0530

    attempt to resolve render issues

--- web-app/src/util/supabase.ts
+++ web-app/src/util/supabase.ts
@@ -1,3 +1,5 @@
+/// <reference types="vite/client" />
+
 import { createClient, RealtimePostgresChangesPayload } from "@supabase/supabase-js"
 import { useCallback, useEffect, useState, } from 'react';
 
